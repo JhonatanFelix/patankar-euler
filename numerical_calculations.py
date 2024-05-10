59e16ee1d123eeb54580318247504bb530edf08c
jhon <jhonyrf@gmail.com>
2024-05-10 14:37:40 +0200

    added more functions to change only the terms

--- numerical_calculations.py
+++ numerical_calculations.py
@@ -54,10 +54,66 @@ def euler_maruyama(pos_funcs, neg_funcs, diffusion_funcs ,times, init_cond):
         negative_increment = sum(d(t, x_current) for d in neg_funcs)
         diffusion_increment = sum(g(t,x_current) for g in diffusion_funcs)
 
-        I = np.random.normal(0 , h, 2)
+        I = np.random.normal(0 , h, len(init_cond))
 
         x[:,i] = x_current + positive_increment - negative_increment + diffusion_increment*I
     
     return x
 
+
+def deterministic_patankar_euler(pos_funcs, neg_funcs, diffusion_funcs ,times, init_cond):
+
+    x = np.zeros((len(init_cond), len(times)))
+    x[:,0] = init_cond
+
+    h = times[1] - times[0]
+
+    for i in range(1, len(times)):
+        t = times[i-1]
+        x_current = x[:, i-1]
+        
+        #positive_increment = np.zeros(i)
+        #negative_increment = np.zeros(i)
+        #diffusion_increment = np.zeros(i)
+
+        positive_increment = sum(f(t, x_current) for f in pos_funcs)
+        negative_increment = sum(d(t, x_current) for d in neg_funcs)
+        diffusion_increment = sum(g(t,x_current) for g in diffusion_funcs)
+        neg_over_x = sum(d(t,x_current)/x_current for d in diffusion_funcs) 
+
+        I = np.random.normal(0 , h, len(init_cond))
+
+        x[:,i] = (x_current + positive_increment*h + diffusion_increment*I)/(1+ neg_over_x) 
+    
+    return x
+
+
+def deterministic_patankar_euler(pos_funcs, neg_funcs, diffusion_funcs ,times, init_cond):
+
+    x = np.zeros((len(init_cond), len(times)))
+    x[:,0] = init_cond
+
+    h = times[1] - times[0]
+
+    for i in range(1, len(times)):
+        t = times[i-1]
+        x_current = x[:, i-1]
+        
+        #positive_increment = np.zeros(i)
+        #negative_increment = np.zeros(i)
+        #diffusion_increment = np.zeros(i)
+
+        positive_increment = sum(f(t, x_current) for f in pos_funcs)
+        #negative_increment = sum(d(t, x_current) for d in neg_funcs)
+        #diffusion_increment = sum(g(t,x_current) for g in diffusion_funcs)
+        neg_over_x = sum(d(t,x_current)/x_current for d in neg_funcs) 
+        diff_over_x = sum(g(t,x_current)/x_current for g in diffusion_funcs)
+
+
+        I = np.random.normal(0 , h, len(init_cond))
+
+        x[:,i] = (x_current + positive_increment*h )/(1+ neg_over_x + neg_over_x - diff_over_x
+                                                    + (iff_over_x)**2 ) 
+    
+    return x
 
